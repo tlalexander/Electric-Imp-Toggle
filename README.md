bba4b8749ad96f4c2567f68ae1627fbf5f2478fa
Taylor Alexander <tlalexander@gmail.com>
2012-08-13 04:32:58 -0700

    initial commit
    made code good and whatnot

---
+++ README.md
@@ -0,0 +1,34 @@
+This app uses HTTP POST to send either a "0" or a "1" to any URL you specify via a QR code scan.
+
+Its designed for use with an electric imp, to make turning something on and off from an Android phone very simple. I'm not a "real" app developer, so look at this as a basic sample app to get started. I don't intend to add much functionality beyond what's here, so please fork this and use as desired! Its licensed Apache, which I think means "go hog wild".
+
+Setting up your imp to receive the request is simple. The absolute most basic imp program you could use this with is this:
+
+----------------------------
+//Http Test example code
+server.log("Started");
+
+class inputHTTP extends InputPort {
+    function set(httpVal) {
+        server.log("Received val "+httpVal);
+    }    
+}
+
+// Register with the server
+imp.configure("httpTest", [inputHTTP()], []);
+// End of code.
+----------------------------
+
+The above code will take a value from an HTTP POST and display it in the server log. To use it, create a new imp code file, name it "http test", paste the contents above (whats in between the dashes) into the code file, and save it. Then go to your planner, hit "add node", and select "HTTP in". You may need to click in the planner to see it, but there should be a new node called "HTTP IN". Select your imp in the planner, click settings at the top right, and load the "http test" code. Then click the plus sign at the top right of the HTTP IN node, and connect it to your imp's node.
+
+Now that you have an HTTP IN node, you can get its custom URL, which you will need to give to the Android app. Doing this is easy. Click on the node, then click the settings in the top right corner. That will display a window that contains a URL like this: https://api.electricimp.com/v1/299e35ee19ed1771/305cef2702105b5f
+
+We will create a QR code with that link that we can scan with this app. To do that, go to http://zxing.appspot.com/generator and in the "contents" dropdown at the top, select "URL". Now go back to the planner, copy the URL from the HTTP IN node settings (which, again, will look like this: https://api.electricimp.com/v1/299e35ee19ed1771/305cef2702105b5f), and paste it into the QR code generator under "URL". Then click "generate", and you should have your QR code.
+
+Open the Android app and hit "Scan Code". You may be directed to download the necessary QR code scanning app from the market. If so, download it, then hit the back button or go home and navigate back to this app, and hit "Scan Code" again. Once the scanner comes up, scan the QR code you generated on the above page. If successful, you should be returned to this app with two new buttons: "Turn On" and "Turn Off". Pressing those will send a 1 or a 0 (respectively) to the electric imp connected to our HTTP IN node. Lets see how that works!
+
+The imp code above code posts to the server (it doesn't turn any LEDs on on your imp or anything), so currently, we need to open the code from the planner to see that. Click on your imp node in the planner, and then click the settings in the top right corner. You should see a link to "edit http test". Click that link, and you should now see the code editor with a log window below. (The way the code editor currently works, you won't get a log window unless you navigate to the code that way. That will change in time, however.)
+
+The log window should say "Started", to indicate your imp is running your code. Hit the "Turn On" button on your Android app, and you should see "Received val 1" in the log window. If you do, everything is working! If not, go back and make sure you did everything properly.
+
+If it worked, there is additional code under the file "hannah.imp" that will toggle an LED on the hannah board. Most of the code is just taken from the hannah default sample, but the bits at the end are modified to take an HTTP request.
---
+++ src/com/tlalexander/imptoggle/ElectricImpTestActivity.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2012 Taylor Alexander
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tlalexander.imptoggle;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.tlalexander.imptest.R;
+
+
+/**
+ * A simple app to toggle on and off a thing connected to your impee. See README.md for more details
+ * 
+ * 
+ * @author Taylor Alexander
+ */
+
+
+
+public class ElectricImpTestActivity extends Activity {
+	
+	
+	private Button buttonOn;
+	private Button buttonOff;
+	private Button buttonScan;
+	private String address;
+	
+	private static final String PREFS_NAME = "PreferenceFile";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        address = settings.getString("address", null);
+        
+        buttonOn = (Button) findViewById(R.id.buttonOn);
+        buttonOn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	   new postDataTask().execute("1");
+            }
+        });
+        
+        buttonOff = (Button) findViewById(R.id.buttonOff);
+        buttonOff.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	   new postDataTask().execute("0");
+     
+            }
+        });
+        
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            	   IntentIntegrator integrator = new IntentIntegrator(ElectricImpTestActivity.this);
+            	   integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+            	   
+            }
+        });
+        
+        if(address==null)
+        {
+        	buttonOn.setVisibility(View.GONE);
+        	buttonOff.setVisibility(View.GONE);
+        
+        }
+        
+        
+    }
+    
+    
+    
+    class postDataTask extends AsyncTask<String, Void, Void> {
+
+        private Exception exception;
+
+        protected Void doInBackground(String... parameter) {
+            try {
+               
+            	  // Perform action on click
+            	HttpClient client = new DefaultHttpClient();
+            	HttpPost post = new HttpPost(address);
+            	
+            	List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+            	pairs.add(new BasicNameValuePair("value",parameter[0]));
+            	//pairs.add(new BasicNameValuePair("key2", "value2"));
+            	try {
+					post.setEntity(new UrlEncodedFormEntity(pairs));
+					client.execute(post);
+					
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            	
+            } catch (Exception e) {
+                this.exception = e;
+            
+                return null;
+            }
+			return null;
+        }
+
+        protected void onPostExecute() {
+            // TODO: check this.exception 
+            // TODO: do something with the feed
+        }
+     
+
+  
+    
+    
+}
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    	  IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+    	  if (scanResult != null) {
+    	    // handle scan result
+    		  
+    		buttonOn.setVisibility(View.VISIBLE);
+          	buttonOff.setVisibility(View.VISIBLE);
+          	
+          	address=scanResult.getContents();
+          	
+          	 // We need an Editor object to make preference changes.
+	        // All objects are from android.context.Context
+	        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	        SharedPreferences.Editor editor = settings.edit();  
+
+	        editor.putString("address", address);
+
+	        // Commit the edits!
+	        editor.commit();
+    		  
+    		  
+    	  }else
+    	  {
+    	  // else continue with any other code you need in the method
+    	  Toast.makeText(getApplicationContext(), R.string.noQRCode, 1000).show();
+    	  }
+    	 
+    	}
+    
+    
+    
+}
+    
+   
